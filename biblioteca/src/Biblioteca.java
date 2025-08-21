9e56890d076023ae5b14d3eb48515d2095cf2a03
bia <anabeatrib@gmail.com>
2025-08-21 15:54:32 -0300

    adicionando a classe livro (aula dia 21/08)

---
+++ biblioteca/src/Biblioteca.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Biblioteca {
+    public static void main(String[] args) {
+        Livro livro1 = new Livro("LPII","Thiago", 2025);
+        livro1.setAno(2024);
+        System.out.println(livro1);
+    }
+}

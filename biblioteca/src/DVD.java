a3ccb27a8639afb0613f0377342a69165fea505a
anabeatrib@gmail.com <anabeatrib@gmail.com>
2025-09-11 23:03:27 -0300

    lista de polimorfismo

---
+++ biblioteca/src/DVD.java
@@ -0,0 +1,33 @@
+public class DVD extends ItemDoAcervo{
+    private int duracaoMinutos;
+
+    public DVD(String titulo, int ano, int duracaoMinutos) {
+        super(titulo, ano);
+        this.duracaoMinutos = duracaoMinutos;
+    }
+    public int getDuracaoMinutos() {
+        return duracaoMinutos;
+    }
+    public void setDuracaoMinutos(int duracaoMinutos) {
+        this.duracaoMinutos = duracaoMinutos;
+    }
+    @Override
+    public int getPrazo() {
+        return 3;
+    }
+
+    @Override
+    public double getValorMultaPorDiaAtraso(){
+        return 2.00;
+    }
+
+    @Override
+    public String toString() {
+        return "DVD{" + "'" +getTitulo()+
+                "' ," + " de (" + getAno() + ")"
+                + " - " + duracaoMinutos + "min" +
+                " - Status: " + getStatus() + '}';
+    }
+}
+
+
